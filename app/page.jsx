f8433a9461b0eb3b335a0d6df43ec834d74465ab
Lilian <lilianpeuron@gmail.com>
2024-02-27 23:59:37 +0100

    init Project

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  color: red;
+`;
+
+export default function Home() {
+  return (
+    <main>
+      <Title>Home</Title>
+    </main>
+  );
+}
